94c27a28affe75e8aee5a5c72ba570d0d43d7598
Morgan Mikel McDaris <morganmcdaris@yahoo.com>
2012-09-11 16:16:20 -0700

    added a file in a subdir

---
+++ lab1/hello.cpp
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  09/11/2012 16:15:27
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  McDaris, Morgan (), mmcdaris@ccsf.edu
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
+
+cout << "yo, u  missing a header"
