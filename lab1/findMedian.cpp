58d9dec581cb1e22498c0ead974c523d4cdaa058
Morgan McDaris <Admin@mmm.local>
2012-09-20 17:30:08 -0700

    In the process of adding while loop to main

--- lab1/findMedian.cpp
+++ lab1/findMedian.cpp
@@ -4,46 +4,82 @@
 
 using namespace std;
 int findMedian (int anArray[], int first, int last, int length);
-void swapIntArray(int anArray[], int x, int y);
+void swapIntArray (int& anArray[], int x, int y);
 
 int main()
 {
-	int array[9] ={ 16, 2, 77, 40, 12, 25,71, 84, 35 };
-	//35 should be median
-	cout << '\n' << findMedian (array, 0, 8, 9);
+	int length; // length of the array
+  bool escape = false;
+  bool odd = false;
+  char yn;
+  
+  while (escape == false)
+  {
+    cout << "Enter the number of items (please enter an odd number): ";
+    cin >> length;
+    if(length%2 == 0 )
+    {
+
+    }
+    else
+    {
+      cout << length << " is not an odd number.";
+    }
+
+  }
+
+
+  } while (escape == false);
+  
+  // int array[9] ={ 16, 2, 77, 40, 12, 25,71, 84, 35 };
+	// 35 should be median
+	// cout << '\n' << findMedian (array, 0, 8, 9);
 	return(0);
 }
 
+/************************************************
+|                  findMedian                   |
+| Finds the Median in an array and returns it!  |
+************************************************/
+
 int findMedian (int anArray[], int first, int last, int length)
 {
-	//Pre Conditions: requires an integer array, requires an array with an odd number of elements
-	//Post Conditions: returns the pivot value if its the median
+	// Pre Conditions: requires an integer array, requires an array with an odd number of elements
+	// Post Conditions: returns the pivot value if its the median
 	// first is the start of the sub array, last is the end of the sub array
 	
-	int pivot = first; //index of pivot
-	int startS1 = first; //starting of S1, which is the index of elements in anArray which are less than the pivot
-	int startS2 = first + 1; //starting of S2, which is the index of elements in anArray which are greater than the pivot, this is also the end of S1
-//	if (first == last)      //if first is equal to last, then we have hit a base case, this occures if there is only one element in the subarray and it is not the median
+	int pivot = first;        // index of pivot
+	int startS1 = first;      // starting of S1, which is the index of elements in anArray which are less than the pivot
+	int endS1 = first + 1;    // starting of S2, which is the index of elements in anArray which are greater than the pivot, this is also the end of S1
+                            
+  // if first is equal to last, then we have hit a base case, this occures if there is only one element in the subarray and it is not the median
 
 	cout<< "false";
-	for(int i = startS1; i<=last; i++) //checks if the value is less than the pivot, if it is then its moved twards the left of the array populating S1
+
+	for(int i = startS1; i<=last; i++) // checks if the value is less than the pivot, if it is then its moved towards the left of the array populating S1
 	{
-		if(anArray[i] < anArray[pivot])
+		if(anArray[i] < anArray[pivot]) // 
 		{
-			swapIntArray(anArray, i, startS2);
-			startS2 = startS2 + 1;
+			swapIntArray(anArray, i, endS1);
+			endS1 = endS1 + 1;
 		}
 	}
-	swapIntArray(anArray, pivot, startS2-1); //startS2 -1 is the last element in the S1 subarray, doing this moves the pivot to the middle
-	pivot = startS2 -1; //used to check for median
+	swapIntArray(anArray, pivot, endS1-1); //endS1 -1 is the last element in the S1 subarray, doing this moves the pivot to the middle
+	pivot = endS1 -1; //used to check for median
 	if (pivot == (length/2)) //length/2 gets the middle of the array
 		return (pivot);
 	else
-		return (findMedian(anArray,first,startS2,length)); //quick sorts S1
-		return (findMedian(anArray,startS2,last,length)); //quick sorts S2
+		return (findMedian(anArray,first,endS1,length)); //quick sorts S1
+		return (findMedian(anArray,endS1,last,length)); //quick sorts S2
 	
 }
 
+/******************************************************
+|                  swapIntArray                       |
+| Takes and array by reference and 2 indexes(x and y) |
+| swaps the values of array[x] and array[y]           |
+| Return: void                                        |
+******************************************************/
 void swapIntArray(int anArray[], int x, int y)
 {
 	//pre : requires an integer array, X and Y must be in the array
@@ -54,6 +90,11 @@ void swapIntArray(int anArray[], int x, int y)
 	anArray[y] = tempX;
 }
 
+/******************************************************
+|                  quickSort                          |
+| pages 479-481 - Quicksort                           |
+| Pass by reference!                                  |
+******************************************************/
 
 /*
 function quicksort('array')
@@ -66,3 +107,4 @@ function quicksort('array')
           else append 'x' to 'greater'
       return concatenate(quicksort('less'), 'pivot', quicksort('greater')) // two recursive calls
 */
+
