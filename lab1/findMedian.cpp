e4728865c0b9cce56d13f8467b3d88f04679e299
Arthur Luk <Psiblade94122>
2012-10-01 23:35:19 -0700

    how do i make it update to my current file?
    see header

--- lab1/findMedian.cpp
+++ lab1/findMedian.cpp
@@ -4,9 +4,10 @@
 #include <string>
 
 using namespace std;
-int findMedian (int anArray[], int first, int last, int length);
-void swapIntArray (int anArray[], int x, int y);
+void quickSort (int &anArray[], int first, int last, int length, bool &medianFound);
+void swapIntArray (int &anArray[], int x, int y);
 void getArray(int anArray[],int length);
+int findMedian(int &anArray[], int first, int last);
 
 
 int main()
@@ -62,34 +63,13 @@ int main()
 | PRE: Int array w/ odd number of elements      |
 | POST: returns the median of the array         |
 ************************************************/
-int findMedian (int anArray[], int first, int last, int length)
+int findMedian(int &anArray[], int first, int last)
 {
-	// first is the start of the sub array, last is the end of the sub array
-	
-	int pivot = first;        // index of pivot
-	int startS1 = first;      // starting of S1, which is the index of elements in anArray which are less than the pivot
-	int endS1 = first + 1;    // starting of S2, which is the index of elements in anArray which are greater than the pivot, this is also the end of S1
-                            
-  // if first is equal to last, then we have hit a base case, this occures if there is only one element in the subarray and it is not the median
-
-	cout<< "false";
-
-	for(int i = startS1; i<=last; i++) // checks if the value is less than the pivot, if it is then its moved towards the left of the array populating S1
-	{
-		if(anArray[i] < anArray[pivot]) // 
-		{
-			swapIntArray(anArray, i, endS1);
-			endS1 = endS1 + 1;
-		}
-	}
-	swapIntArray(anArray, pivot, endS1-1); //endS1 -1 is the last element in the S1 subarray, doing this moves the pivot to the middle
-	pivot = endS1 -1; //used to check for median
-	if (pivot == (length/2)) //length/2 gets the middle of the array
-		return (pivot);
-	else
-		return (findMedian(anArray,first,endS1,length)); //quick sorts S1
-		return (findMedian(anArray,endS1,last,length)); //quick sorts S2
-	
+	int median;
+	bool medianFound = false;
+	quickSort(anArray, first, last, (last - first), medianFound);
+	median = anArray[((first+last)/2)];
+	return(median);
 }
 
 /******************************************************
@@ -98,7 +78,7 @@ int findMedian (int anArray[], int first, int last, int length)
 | swaps the values of array[x] and array[y]           |
 | Return: void                                        |
 ******************************************************/
-void swapIntArray(int anArray[], int x, int y)
+void swapIntArray(int &anArray[], int x, int y)
 {
 	//pre : requires an integer array, X and Y must be in the array
 	//post: swaps the X and Y elements of anArray
@@ -125,6 +105,39 @@ function quicksort('array')
           else append 'x' to 'greater'
       return concatenate(quicksort('less'), 'pivot', quicksort('greater')) // two recursive calls
 */
+void quickSort (int &anArray[], int first, int last, int length, bool &medianFound)
+{
+	// first is the start of the sub array, last is the end of the sub array
+	
+	int pivot = first;        // index of pivot
+	int startS1 = first +1;      // starting of S1, which is the index of elements in anArray which are less than the pivot
+	int endS1 = first +1;    // starting of S2, which is the index of elements in anArray which are greater than the pivot, this is also the end of S1
+                            
+  // if first is equal to last, then we have hit a base case, this occures if there is only one element in the subarray and it is not the median
+
+	if((first => last) || (medianFound)) //median found stops the function from recursing if the median has been found in a previous recursion
+	{
+			return();
+	}
+
+	for(int i = startS1; i<=last; i++) // checks if the value is less than the pivot, if it is then its moved towards the left of the array populating S1
+	{
+		if(anArray[i] < anArray[pivot]) // 
+		{
+			swapIntArray(anArray, i, endS1);
+			endS1 = endS1 + 1;
+		}
+	}
+	swapIntArray(anArray, pivot, endS1-1); //endS1 -1 is the last element in the S1 subarray, doing this moves the pivot to the middle
+	pivot = endS1 -1; //used to check for median
+	if (pivot == (length/2)) //length/2 gets the middle of the array
+		 medianFound = true;
+		 return();
+	else
+		quickSort(anArray, first, endS1, length, medianFound); //quick sorts S1
+		quickSort(anArray, endS1, last, length, medianFound); //quick sorts S2
+	
+}
 
 
 /******************************************************
